4f948c91528e0771c819aacf762dbd1c975fc3e2
Simon Jakobi <simon.jakobi@gmail.com>
2018-02-18 01:23:36 +0100

    Update the testcase
    As there can only be a single DEFAULT alternative, we want to combine
    it from the largest set of alts.

--- testsuite/tests/simplCore/should_compile/T14684.hs
+++ testsuite/tests/simplCore/should_compile/T14684.hs
@@ -1,17 +1,21 @@
 -- This function currently doesn't simplify using the
--- combine-identical-alternatives optimisation
+-- combine-identical-alternatives optimisation.
+-- After the fix, Foo3 {}, Foo5 and Foo6 should be combined
+-- to a single DEFAULT alternative.
+
 
 module T14684 where
 
 import GHC.List as L
 
-data Foo = Foo1 | Foo2 | Foo3 !Int | Foo4 | Foo5
+data Foo = Foo1 | Foo2 | Foo3 !Int | Foo4 | Foo5 | Foo6
 
 fun1 :: Foo -> Int
 {-# NOINLINE fun1 #-}
 fun1 x = case x of
                Foo1 -> 0
                Foo2 -> 1
-               Foo3 {} -> 1
-               Foo4 -> 2
+               Foo3 {} -> 2
+               Foo4 -> 1
                Foo5 -> 2
+               Foo6 -> 2
