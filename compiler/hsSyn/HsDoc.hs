9f7b387c13a0008027e89ee1a91eb09504778a8e
Simon Jakobi <simon.jakobi@gmail.com>
2018-05-23 22:59:57 +0200

    [No-TC] Make mkMaps typecheck

--- compiler/hsSyn/HsDoc.hs
+++ compiler/hsSyn/HsDoc.hs
@@ -1,8 +1,9 @@
-{-# LANGUAGE BangPatterns, CPP, DeriveDataTypeable #-}
+{-# LANGUAGE BangPatterns, CPP, DeriveDataTypeable, GeneralizedNewtypeDeriving #-}
 
 module HsDoc (
   HsDoc(..),
   LHsDoc,
+  concatHsDoc,
   HsDocString(..),
   LHsDocString,
   mkHsDocString,
@@ -20,7 +21,8 @@ module HsDoc (
 import GhcPrelude
 
 import Name
-import Outputable
+import Outputable hiding ((<>))
+import qualified Outputable
 import SrcLoc
 import FastString
 import Binary
@@ -29,6 +31,7 @@ import Data.Data
 import Data.Foldable
 import Data.Map (Map)
 import qualified Data.Map as Map
+import Data.Semigroup
 
 data HsDocIdentifierSpan = HsDocIdentifierSpan !Int !Int
   deriving (Eq, Show, Data)
@@ -43,13 +46,21 @@ instance Binary HsDocIdentifierSpan where
     return (HsDocIdentifierSpan a b)
 
 instance Outputable HsDocIdentifierSpan where
-  ppr (HsDocIdentifierSpan a b) = int a <> char '-' <> int b
+  ppr (HsDocIdentifierSpan a b) = int a Outputable.<> char '-' Outputable.<> int b
+
+shiftHsDocIdentifierSpan :: Int -> HsDocIdentifierSpan -> HsDocIdentifierSpan
+shiftHsDocIdentifierSpan n (HsDocIdentifierSpan a b) =
+  HsDocIdentifierSpan (a + n) (b + n)
 
 data HsDocIdentifier name = HsDocIdentifier
   { hsDocIdentifierSpan :: !HsDocIdentifierSpan
   , hsDocIdentifierString :: !HsDocString
   , hsDocIdentifierNames :: ![name]
   } deriving (Eq, Show, Data)
+
+shiftHsDocIdentifier :: Int -> HsDocIdentifier name -> HsDocIdentifier name
+shiftHsDocIdentifier n (HsDocIdentifier span s names) =
+  HsDocIdentifier (shiftHsDocIdentifierSpan n span) s names
 
 data HsDoc name = HsDoc
   { hsDocString :: !HsDocString
@@ -59,21 +70,43 @@ data HsDoc name = HsDoc
 instance Outputable (HsDoc a) where
   ppr _ = text "<document comment>"
 
+instance Semigroup (HsDoc a) where
+  -- Do I need to add an extra '\n' in between?
+  HsDoc s0 ids0 <> HsDoc s1 ids1 =
+    HsDoc (s0 <> s1)
+          (ids0 ++ map (shiftHsDocIdentifier (lengthHDS s0)) ids1)
+
+instance Monoid (HsDoc a) where
+  mempty = HsDoc mempty []
+  mappend = (<>)
+
+concatHsDoc :: [HsDoc name] -> Maybe (HsDoc name)
+concatHsDoc xs =
+  case mconcat xs of
+    HsDoc s [] | nullHDS s -> Nothing
+    x -> Just x
+
 type LHsDoc name = Located (HsDoc name)
 
 -- | Haskell Documentation String
 newtype HsDocString = HsDocString FastString
-  deriving (Eq, Ord, Show, Data)
+  deriving (Eq, Ord, Show, Data, Semigroup, Monoid)
 
 instance Binary HsDocString where
   put_ bh (HsDocString fs) = put_ bh fs
   get bh = HsDocString <$> get bh
 
+nullHDS :: HsDocString -> Bool
+nullHDS (HsDocString fs) = nullFS fs
+
+lengthHDS :: HsDocString -> Int
+lengthHDS (HsDocString fs) = lengthFS fs
+
 -- | Located Haskell Documentation String
 type LHsDocString = Located HsDocString
 
 instance Outputable HsDocString where
-  ppr (HsDocString fs) = char '"' <> ftext fs <> char '"'
+  ppr (HsDocString fs) = char '"' Outputable.<> ftext fs Outputable.<> char '"'
 
 mkHsDocString :: String -> HsDocString
 mkHsDocString = HsDocString . mkFastString
@@ -92,7 +125,7 @@ instance Binary HsDocNamesMap where
 instance Outputable HsDocNamesMap where
   ppr (HsDocNamesMap m) = vcat (map pprPair (Map.toAscList m))
     where
-      pprPair (s, names) = ppr s <> colon $$ nest 2 (vcat (map pprName' names))
+      pprPair (s, names) = ppr s Outputable.<> colon $$ nest 2 (vcat (map pprName' names))
       pprName' n = ppr (nameOccName n) <+> text "from" <+> ppr (nameModule n)
 
 emptyHsDocNamesMap :: HsDocNamesMap
