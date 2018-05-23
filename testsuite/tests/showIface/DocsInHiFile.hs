43b3059d0bfb616cdee9d20aed1367759eebdd03
Simon Jakobi <simon.jakobi@gmail.com>
2018-05-23 12:31:23 +0200

    DocsInHiFile test: Add declarations and docs

--- testsuite/tests/showIface/DocsInHiFile.hs
+++ testsuite/tests/showIface/DocsInHiFile.hs
@@ -4,5 +4,16 @@
 -}
 module DocsInHiFile where
 
+-- | '()', 'elem'.
 elem :: ()
 elem = ()
+
+-- | A datatype.
+data D
+  = D0 -- ^ A constructor for 'D'. '
+  | D1 -- ^ Another constructor
+
+add :: Int -- ^ First summand for 'add'
+    -> Int -- ^ Second summand
+    -> Int -- ^ Sum
+add a b = a + b
