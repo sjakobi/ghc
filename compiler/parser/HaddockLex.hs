de40c0565534c4673f6ebad9d86dcd2598095fa6
Simon Jakobi <simon.jakobi@gmail.com>
2018-05-24 18:36:47 +0200

    HaddockLex: Misc changes

--- compiler/parser/HaddockLex.hs
+++ compiler/parser/HaddockLex.hs
@@ -21,12 +21,16 @@ import Data.Maybe
 import Text.Parsec (Stream, ParsecT)
 import qualified Text.Parsec as P
 
-lexHsDoc :: P (GenLocated loc RdrName) -> String -> HsDoc RdrName
+-- | Lex identifiers from a docstring.
+lexHsDoc :: P (GenLocated loc RdrName) -- ^ A precise identifier parser
+         -> String                     -- ^ A docstring
+         -> HsDoc RdrName
 lexHsDoc parseIdentifier s = HsDoc (mkHsDocString s) (mapMaybe maybeDocIdentifier idxdPlausIds)
   where
     maybeDocIdentifier :: (Int, String, Int) -> Maybe (HsDocIdentifier RdrName)
     maybeDocIdentifier (ix0, pid, ix1) =
-      HsDocIdentifier (HsDocIdentifierSpan ix0 ix1) (mkHsDocString pid) . (: []) <$> parseIdent parseIdentifier pid
+      HsDocIdentifier (HsDocIdentifierSpan ix0 ix1) (mkHsDocString pid) . (: [])
+        <$> parseIdent parseIdentifier pid
     idxdPlausIds =
       either (error . show)
              id
@@ -84,9 +88,9 @@ isIdentChar c = not (isSpace c) && c /= '`'
 -- adapted from haddock-api
 parseIdent :: P (GenLocated loc RdrName) -> String -> Maybe RdrName
 parseIdent parseIdentifier str0 =
-  let pflags = ParserFlags EnumSet.empty EnumSet.empty (stringToUnitId "unknown package") 0
+  let pflags = ParserFlags EnumSet.empty EnumSet.empty (stringToUnitId "") 0
       buffer = stringToStringBuffer str0
-      realSrcLc = mkRealSrcLoc (mkFastString "<unknown file>") 0 0
+      realSrcLc = mkRealSrcLoc (mkFastString "") 0 0
       pstate = mkPStatePure pflags buffer realSrcLc
   in case unP parseIdentifier pstate of
     POk _ name -> Just (unLoc name)
