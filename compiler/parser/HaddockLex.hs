e68b30c5bd1b93b879a87a3a8be7ee064afea926
Simon Jakobi <simon.jakobi@gmail.com>
2018-05-26 01:22:25 +0200

    HaddockLex: Cleanup

--- compiler/parser/HaddockLex.hs
+++ compiler/parser/HaddockLex.hs
@@ -20,78 +20,100 @@ import Data.Maybe
 
 import Text.Parsec (Stream, ParsecT)
 import qualified Text.Parsec as P
+import qualified Text.Parsec.Pos as P
 
 -- | Lex identifiers from a docstring.
 lexHsDoc :: P (GenLocated loc RdrName) -- ^ A precise identifier parser
          -> String                     -- ^ A docstring
          -> HsDoc RdrName
-lexHsDoc parseIdentifier s = HsDoc (mkHsDocString s) (mapMaybe maybeDocIdentifier idxdPlausIds)
+lexHsDoc identParser s =
+    HsDoc (mkHsDocString s) (mapMaybe maybeDocIdentifier plausibleIdents)
   where
     maybeDocIdentifier :: (Int, String, Int) -> Maybe (HsDocIdentifier RdrName)
     maybeDocIdentifier (ix0, pid, ix1) =
       HsDocIdentifier (HsDocIdentifierSpan ix0 ix1) (mkHsDocString pid) . (: [])
-        <$> parseIdent parseIdentifier pid
-    idxdPlausIds =
+        <$> validateIdentWith identParser pid
+
+    plausibleIdents :: [(Int, String, Int)]
+    plausibleIdents =
       either (error . show)
              id
-             (P.runParser (identifiersWith (delimited plausibleIdentifierWithIndices))
+             (P.runParser (setOffset 0
+                           *> identsWith (delimited (withOffsets plausibleIdent)))
                           () "" s)
 
-identifiersWith :: Stream s m Char => ParsecT s u m a -> ParsecT s u m [a]
-identifiersWith identifier =
-    catMaybes <$> P.many (P.try (Just <$> identifier) <|> handleNewline <|> dropDelim <|> dropUntilDelim)
+identsWith :: Stream s m Char => ParsecT s u m a -> ParsecT s u m [a]
+identsWith p =
+    catMaybes
+      <$> P.many (P.try (Just <$> p)
+                  <|> handleNewline
+                  <|> dropDelim
+                  <|> dropUntilDelimOrNewline
+                 )
   where
+    -- Increment the offset (actually the column position) instead of having it
+    -- to 0.
     handleNewline = do
-      p0 <- P.getPosition
+      off <- getOffset
       _ <- P.char '\n'
-      P.setPosition (P.incSourceColumn p0 1)
-      return Nothing
-    dropUntilDelim = P.skipMany1 (P.satisfy (\c -> not (isDelim c) && c /= '\n')) $> Nothing
-    dropDelim = identDelim $> Nothing
+      setOffset (succ off)
+      pure Nothing
 
-plausibleIdentifierWithIndices :: Stream s m Char => ParsecT s u m (Int, String, Int)
-plausibleIdentifierWithIndices = liftA3 (,,) getColPos plausibleIdentifier getColPos
-  where
-    -- Instead of subtracting 1 from the sourceColumn
-    -- we could use P.setPosition to start at 0.
-    getColPos = (pred . P.sourceColumn) <$> P.getPosition
+    dropDelim = delim $> Nothing
 
-delimited :: Stream s m Char => ParsecT s u m a -> ParsecT s u m a
-delimited = P.between identDelim identDelim
+    dropUntilDelimOrNewline =
+      P.skipMany1 (P.satisfy (\c -> not (isDelim c || c == '\n'))) $> Nothing
 
-plausibleIdentifier :: Stream s m Char => ParsecT s u m String
-plausibleIdentifier = do
-    c <- P.satisfy isFirstIdentChar
+-- | An overly permissive identifier parser
+plausibleIdent :: Stream s m Char => ParsecT s u m String
+plausibleIdent = do
+    c <- identStart
     cs <- p
     return (c : cs)
   where
     p = do
-      vs <- many (P.satisfy (\c -> isIdentChar c && c /= '\''))
+      vs <- many identExcept'
       c <- P.lookAhead P.anyChar
       case c of
         '`' -> return vs
         '\'' -> P.try ((\x -> vs ++ "'" ++ x) <$> (P.char '\'' *> p)) <|> return vs
-        _ -> fail "outofvalid"
+        _ -> fail "Not a delimiter"
 
-identDelim :: Stream s m Char => ParsecT s u m Char
-identDelim = P.satisfy isDelim
+    identStart = P.letter <|> P.char '_' <|> symbol
+    identExcept' = P.satisfy isAlphaNum <|> P.char '_' <|> symbol
+    symbol = asciiSymbol <|> unicodeSymbol
+    asciiSymbol = P.oneOf "!#$%&⋆+./<=>?@\\^|-~:"
+    unicodeSymbol = P.satisfy (\c -> not (isAscii c) && isSymbol c)
+
+withOffsets :: Stream s m Char => ParsecT s u m a -> ParsecT s u m (Int, a, Int)
+withOffsets p = liftA3 (,,) getOffset p getOffset
+
+delimited :: Stream s m Char => ParsecT s u m a -> ParsecT s u m a
+delimited = P.between delim delim
+
+delim :: Stream s m Char => ParsecT s u m Char
+delim = P.satisfy isDelim
 
 isDelim :: Char -> Bool
 isDelim c = c == '\'' || c == '`'
 
-isFirstIdentChar :: Char -> Bool
-isFirstIdentChar c = (isAlpha c || c == '_' || isSymbol c || c == ':') && not (isDelim c)
-
-isIdentChar :: Char -> Bool
-isIdentChar c = not (isSpace c) && c /= '`'
-
--- adapted from haddock-api
-parseIdent :: P (GenLocated loc RdrName) -> String -> Maybe RdrName
-parseIdent parseIdentifier str0 =
+validateIdentWith :: P (GenLocated loc RdrName) -> String -> Maybe RdrName
+validateIdentWith identParser str0 =
   let pflags = ParserFlags EnumSet.empty EnumSet.empty (stringToUnitId "") 0
       buffer = stringToStringBuffer str0
       realSrcLc = mkRealSrcLoc (mkFastString "") 0 0
       pstate = mkPStatePure pflags buffer realSrcLc
-  in case unP parseIdentifier pstate of
+  in case unP identParser pstate of
     POk _ name -> Just (unLoc name)
     _ -> Nothing
+
+--------------------------------------------------------------------------------
+-- Offset handling
+
+-- We use parsec's 'P.getPosition' and 'P.setPosition' to track our offset
+-- from the beginning of the stream.
+setOffset :: Monad m => Int -> ParsecT s u m ()
+setOffset off = P.setPosition (P.newPos "" 0 off)
+
+getOffset :: Monad m => ParsecT s u m Int
+getOffset = P.sourceColumn <$> P.getPosition
